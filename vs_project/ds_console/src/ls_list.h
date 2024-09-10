190e36b749ec4559fe6b19e2b9b0f66ee403ba6a
lhy <lianghongyu.csu@gmail.com>
2024-09-10 11:31:58 +0800

    添加vs工程，方便调试

---
+++ vs_project/ds_console/src/ls_list.h
@@ -0,0 +1,99 @@
+﻿#pragma once
+
+#include <stdbool.h>
+
+// 节点结构体定义
+typedef struct _tag_lsNode
+{
+    struct _tag_lsNode *next;// 节点指针域  note : 这里涉及到不完全类型指针的使用
+    int data;// 节点数据域
+} lsNode;
+
+/**
+ * @brief 节点创建
+ * 
+ * @param data 节点数据
+ * @return lsNode* 返回节点指针
+ */
+lsNode *ls_node_create(int data);
+
+// 链表结构体定义
+typedef struct
+{
+    lsNode *head;// 链表内部维护的头节点
+} lsList;
+
+/**
+ * @brief 链表创建
+ * 
+ * @return lsList* 返回链表指针
+ */
+lsList *ls_list_create();
+
+/**
+ * @brief 链表销毁
+ * 
+ * @param root 链表的地址
+ */
+void ls_list_destroy(lsList **root);
+
+/**
+ * @brief 链表尾部追加节点
+ * 
+ * @param list 链表指针
+ * @param data 数据
+ */
+void ls_list_append(const lsList *list, int data);
+
+/**
+ * @brief 链表头部追加节点
+ * 
+ * @param list 链表指针
+ * @param data 数据
+ */
+void ls_list_head_append(const lsList *list, int data);
+
+/**
+ * @brief 链表打印
+ * 
+ * @param list 链表指针
+ */
+void ls_list_print(const lsList *list);
+
+// 链表迭代器
+typedef struct
+{
+    lsNode *current;
+} lsListIterator;
+
+/**
+ * @brief 创建并初始化一个链表迭代器
+ * 
+ * @param list 需要迭代的链表指针
+ * @return lsListIterator 返回迭代器
+ */
+lsListIterator ls_list_iterator_start(const lsList *list);
+
+/**
+ * @brief 判断迭代器是否到达链表尾节点之后
+ * 
+ * @param it 迭代器指针
+ * @return true 迭代器已到达尾节点之后
+ * @return false 迭代器未到达尾节点之后
+ */
+bool ls_list_iterator_done(lsListIterator *it);
+
+/**
+ * @brief 迭代器前进一个节点
+ * 
+ * @param it 迭代器指针
+ */
+void ls_list_iterator_step(lsListIterator *it);
+
+/**
+ * @brief 通过迭代器获取当前节点数据
+ * 
+ * @param it 迭代器指针
+ * @return void* 返回节点数据指针
+ */
+void *ls_list_iterator_get_data(lsListIterator *it);
---
+++ vs_project/ds_console/test/test_ls_list.c
@@ -0,0 +1,50 @@
+﻿#include "../src/ls_list.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void test_list_basic()
+{
+    lsList *list = ls_list_create();
+
+    ls_list_append(list, 1);
+    ls_list_append(list, 2);
+    ls_list_append(list, 3);
+    ls_list_append(list, 4);
+    ls_list_head_append(list,5);
+    ls_list_head_append(list,6);
+
+    ls_list_print(list);
+
+    ls_list_destroy(&list);
+
+    ls_list_print(list);// 再次打印，因链表已经销毁，所以没有输出了
+}
+
+void test_list_iterator()
+{
+    lsList *list = ls_list_create();
+
+    ls_list_append(list, 1);
+    ls_list_append(list, 2);
+    ls_list_append(list, 3);
+    ls_list_append(list, 4);
+
+    // 用迭代器遍历链表，自定义打印
+    for (lsListIterator it = ls_list_iterator_start(list); !ls_list_iterator_done(&it); ls_list_iterator_step(&it))
+    {
+        // 可以参考这种取数据的方式，实现往链表上挂载任意数据
+        // 即，数据域存储指向数据的指针，需要获取数据的时候，根据这个指针读取数据
+        const int *pData = ls_list_iterator_get_data(&it);
+        printf("node data : %d\n", *pData);
+    }
+
+    ls_list_destroy(&list);
+}
+
+int main()
+{
+    test_list_iterator();
+
+    return 0;
+}
