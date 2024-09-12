dca6263cae7233eae5894c4fa8f1a60434a98d75
lwj10024 <liangwenju10086@outlook.com>
2024-09-12 14:38:15 +0800

    基础的api调用绘制图形

--- vs_project/ds_console/ls_window.c
+++ vs_project/ds_console/ls_window.c
@@ -14,12 +14,35 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
-        // 在这里进行绘图操作
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+        // 在这里进行绘图操作(创建、选择、使用、恢复、销毁)
+        HPEN hPen = CreatePen(PS_SOLID,2,RGB(255,0,0));//画笔
+        HGDIOBJ oldPen = SelectObject(hdc,hPen);
 
+        HBRUSH hBrush = CreateSolidBrush(RGB(0,255,0));
+        HGDIOBJ oldBursh = SelectObject(hdc,hBrush);
+
+        //绘制矩形
+        Rectangle(hdc,50,50,100,100);
+
+        //绘制椭圆
+        Ellipse(hdc,200,50,400,150);
+
+        //绘制直线
+        MoveToEx(hdc,50,200,NULL);//起点
+        LineTo(hdc,200,300);
+
+        SelectObject(hdc,oldPen);
+        SelectObject(hdc,oldBursh);
+
+
+        DeleteObject(hPen);
+        DeleteObject(hBrush);
+ 
         EndPaint(hwnd, &ps);
+
+        return 0;
     }
-    return 0;
+ 
 
     // 调用默认的窗口过程
     default:
