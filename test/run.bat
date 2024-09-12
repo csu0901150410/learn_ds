739f3e7bf125f6f2ee700bf9b2bc49f0df267de6
lhy <lianghongyu.csu@gmail.com>
2024-09-12 23:41:16 +0800

    删除多余文件

--- test/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-echo Compiling test_ls_entity.c
-gcc test_ls_entity.c ../src/ls_list.c ../src/ls_vector.c ../src/ls_line_segment.c ../src/ls_arc.c ../src/ls_entity.c -o test_ls_entity
-
-echo Running test_ls_entity.exe
-test_ls_entity.exe
